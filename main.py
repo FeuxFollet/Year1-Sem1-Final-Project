c3693c04f42355ed9ad721d4c6860a9c61e83fa4
Feux Follets <phirawitsommai@gmail.com>
2025-12-14 13:54:07 +0700

    Toggle side bar implemented

---
+++ main.py
@@ -0,0 +1,121 @@
+#-----------------------------------------------------------------------------#
+# Modules
+
+import tkinter as tk
+import requests
+import numpy as np
+import matplotlib
+
+#-----------------------------------------------------------------------------#
+# Creating Main Window
+
+root = tk.Tk()
+root.title("Project ORBIT")
+root.geometry("960x540")
+root.config(bg="#393939")
+
+#-----------------------------------------------------------------------------#
+# Top welcome message
+
+welcomemsg = tk.Label(
+    root,
+    text="Welcome to ORBIT Cryptotracker",
+    font=("Helvetica", 12, "bold"),
+    foreground="#00bf63",
+    background="#393939"
+)
+welcomemsg.pack(pady=(10, 10))
+
+#-----------------------------------------------------------------------------#
+# Toggler and dashboard frame
+
+toggler_and_dashboard = tk.Frame(root, bg="#393939")
+toggler_and_dashboard.pack(fill="both", expand=True)
+
+#-----------------------------------------------------------------------------#
+# Toggler frame (for interactive currency toggler)
+
+# Frame for the toggler
+grid_frame = tk.Frame(toggler_and_dashboard, background="#393939")
+grid_frame.pack(side="left", fill="y", padx=15)
+
+# 3 Column grid
+grid_frame.columnconfigure(0, weight=1)  # Display currency name
+grid_frame.columnconfigure(1, weight=1)  # Price toggle button
+grid_frame.columnconfigure(2, weight=1)  # Detailed view button
+
+togglerlabel = tk.Label(
+    grid_frame,
+    text="Currency toggle",
+    font=("Helvetica", 12, "bold"),
+    foreground="#00bf63",
+    background="#393939"
+)
+togglerlabel.grid(row=0, column=0, sticky="nsew", padx=10, pady=5)
+
+class CurrencyRow:
+    '''Reusable interactive currency toggler row'''
+
+    def __init__(self, parent, row, name):
+        # Currency label
+        self.displaytext = tk.Label(
+            parent,
+            text=name,
+            font=("Helvetica", 10, "bold"),
+            borderwidth=3,
+            relief=tk.SUNKEN,
+            background="#00bf63",
+            foreground="#01600a",
+            padx=10
+        )
+
+        # Toggle price button
+        self.btn_left = tk.Button(
+            parent,
+            text="Toggle price",
+            font=("Helvetica", 10),
+            background="#606060",
+            foreground="White",
+            activebackground="#323232",
+            activeforeground="Grey",
+            padx=10,
+        )
+
+        # Show graph button
+        self.btn_right = tk.Button(
+            parent,
+            text="Detailed display",
+            font=("Helvetica", 10),
+            background="#606060",
+            foreground="White",
+            activebackground="#323232",
+            activeforeground="Grey",
+            padx=10
+        )
+
+        # Grid all the components
+        self.displaytext.grid(row=row, column=0, sticky="nsew", padx=10, pady=5)
+        self.btn_left.grid(row=row, column=1, sticky="nsew", padx=10, pady=5)
+        self.btn_right.grid(row=row, column=2, sticky="nsew", padx=10, pady=5)
+        self.is_visible = False
+
+
+    def set_price_toggle(self, command):
+        '''Set the command for the price toggle button'''
+        self.btn_left.config(command=command)
+
+
+    def set_detailed_view(self, command):
+        '''Set the command for the detailed view button'''
+        self.btn_right.config(command=command)
+
+
+# Create all the interactive currency toggler
+BTCtoggle = CurrencyRow(grid_frame, row=1, name="Bitcoin (BTC)")
+ETHtoggle = CurrencyRow(grid_frame, row=2, name="Ether (ETH)")
+SOLtoggle = CurrencyRow(grid_frame, row=3, name="Solana (SOL)")
+DOGEtoggle = CurrencyRow(grid_frame, row=4, name="Dogecoin (DOGE)")
+SHIBtoggle = CurrencyRow(grid_frame, row=5, name="Shiba inu (SHIB)")
+
+# Start main loop
+root.mainloop()
